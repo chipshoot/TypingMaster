1c3344963b0ead09ab3749e7b59b6b6a1d2ea326
Chaoyang Fang <fchy@outlook.com>
2025-01-29 23:32:12 -0500

    working version of practice, need fix the issue of space highlight

--- TypingMaster.Client/Program.cs
+++ TypingMaster.Client/Program.cs
@@ -1,3 +1,4 @@
+using BootstrapBlazor.Components;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using TypingMaster.Client;
@@ -8,4 +9,5 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+builder.Services.AddBootstrapBlazor();
 await builder.Build().RunAsync();
