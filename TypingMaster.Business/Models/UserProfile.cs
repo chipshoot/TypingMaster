a1cb160d294c63ab400502fb78828e974eb4bce4
Chaoyang Fang <fchy@outlook.com>
2025-03-08 14:16:53 -0500

    Add Avatar to user profile

--- TypingMaster.Business/Models/UserProfile.cs
+++ TypingMaster.Business/Models/UserProfile.cs
@@ -3,6 +3,12 @@
 public class UserProfile
 {
     public string? FirstName { get; set; }
+
     public string LastName { get; set; } = null!;
+
     public string? Title { get; set; }
+
+    public string? PhoneNumber { get; set; }
+
+    public string? AvatarUrl { get; set; }
 }
