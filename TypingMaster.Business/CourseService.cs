67e8c3d9f6bf957e5c216a03a85ca50992d1dad8
Chaoyang Fang <fchy@outlook.com>
2025-04-07 10:35:05 -0400

    update course service for practice course

--- TypingMaster.Business/CourseService.cs
+++ TypingMaster.Business/CourseService.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Configuration;
 using Serilog;
 using TypingMaster.Business.Contract;
 using TypingMaster.Business.Course;
+using TypingMaster.Core.Constants;
 using TypingMaster.Core.Models;
 using TypingMaster.Core.Models.Courses;
 using TypingMaster.DataAccess.Dao;
@@ -14,68 +15,22 @@ public class CourseService(ICourseRepository courseRepository, IAccountRepositor
     : ServiceBase(logger), ICourseService
 {
     private readonly CourseFactory _courseFactory = new CourseFactory(logger);
-    public static Guid CourseId1 = new("AB7E8988-4E54-435F-9DC3-25D3193EC378");
 
-    // todo: create a new way to get speed test course
-    // todo: move the text to a JSON data file
-    //course = new AdvancedLevelCourse()
-    //{
-    //    Id = AllKeyTestCourseId,
-    //    Lessons =
-    //        [
-    //            new Lesson
-    //            {
-    //                Id = 1,
-    //                PracticeText = "hAnother sharp-witted character, _;  The aerialist's phone number was (217) 389-7557.  The freeloaders expanded by this den located at 97191 Straus Square, Piscataway, Ohio 67107-0614! \"Boy!\" she vocalized.  This understandably zealous physician was jesting regarding my executive around 23 Heyl Plaza, Quantico, Alabama 92780-8450!  The jaywalkers hurled beside the piazza located at 57 Arrington Center, Hatfield, DE",
-    //                Point = 1,
-    //                Description = "The course advances to the next level of lessons if the current typing performance level is equal to or above the advanced level."
-    //            },
-    //            new Lesson
-    //            {
-    //                Id = 2,
-    //                PracticeText = "Taking a test will give this program a basic for customizing individual lessons. Don't be concerned if you aren't typing as quickly as you can; you will soon excel.  Everyone has a quandary with number and symbol keys.  Don't expect to zoom through these next sentences - just do your best! \"Her birthday is 08/154/53.\"  Does 73% of $546.00 = $98.58? Item #32 (jonquils)  sold 2 units @ $6 & 1 @ $73. *+* That's all folks! +*+",
-    //                Point = 1,
-    //                Description = "The course advances to the next level of lessons if the current typing performance level is equal to or above the advanced level."
-    //            },
-    //            new Lesson
-    //            {
-    //                Id = 3,
-    //                PracticeText = "SHIFTING thoughts, typing FLUENTLY, developing SPEED! Caps Lock may be a friend or foe—KNOW when to use it. Function keys (F1-F12) aid shortcuts. Ctrl + C copies, Ctrl + V pastes, and Alt + Tab swaps. The Escape key, a way out. Don't ignore the power of the ENTER key—confirming, submitting, breaking lines.  Every key counts, so practice with purpose!",
-    //                Point = 1,
-    //                Description = "The course advances to the next level of lessons if the current typing performance level is equal to or above the advanced level."
-    //            },
-    //            new Lesson
-    //            {
-    //                Id = 4,
-    //                PracticeText = "Coding requires symbols: {} for blocks, [] for arrays, <> for comparisons. The backslash \\ is essential in file paths, escaping characters. Quotes \"\" and '' frame strings, while ` allows inline code. The spacebar, a silent hero, controls rhythm. Typing, like music, has a flow—staccato or smooth.  To improve, practice! Type, correct, repeat, master.",
-    //                Point = 2,
-    //                Description = "The course advances to the next level of lessons if the current typing performance level is equal to or above the advanced level."
-    //            },
-    //            new Lesson
-    //            {
-    //                Id = 5,
-    //                PracticeText = "Emails need @, URLs need .com, passwords mix Aa1! Common shortcuts: Ctrl + S saves, Ctrl + Z undoes, Ctrl + X cuts. Mac users prefer Cmd over Ctrl. The numpad—quicker digits, faster math. Ergonomic posture boosts accuracy. Fingers rest on ASDF JKL;—home row anchors speed. Keep going, improving, perfecting!  Type on, challenge yourself, enjoy the process.",
-    //                Point = 3,
-    //                Description = "The course advances to the next level of lessons if the current typing performance level is equal to or above the advanced level."
-    //            },
-    //            new Lesson
-    //            {
-    //                Id = 6,
-    //                PracticeText = "Practice drills: Type \"The $ sign matters\"—spot it? Press Tab \u2192 it indents. Press Backspace \u2190 it erases. Use Delete \u27a1 it clears ahead. Adjust Caps \u21ea if needed. Special characters? Try Alt + 0153 (\u2122) or Alt + 0169 (\u00a9). Ever typed in different languages? ¡Sí! Voilà! 汉字也行. Typing unites, connects, empowers.  Never stop—keep practicing!",
-    //                Point = 4,
-    //                Description = "The course advances to the next level of lessons if the current typing performance level is equal to or above the advanced level."
-    //            }
-    //        ]
-    //};
-    //_courses.Add(course);
+    public static Guid CourseId1 = new("AB7E8988-4E54-435F-9DC3-25D3193EC378");
 
     public async Task<CourseDto?> GetCourse(Guid id)
     {
         try
         {
             var courseDao = await courseRepository.GetCourseByIdAsync(id);
+            if (courseDao == null)
+            {
+                return null;
+            }
+
             var course = mapper.Map<CourseDto>(courseDao);
             return course;
+
         }
         catch (Exception ex)
         {
@@ -84,46 +39,60 @@ public class CourseService(ICourseRepository courseRepository, IAccountRepositor
         }
     }
 
-    public async Task<CourseDto?> GetAllKeysCourse(Guid id)
+    public async Task<IEnumerable<CourseDto>> GetCoursesByType(int accountId, TrainingType type)
     {
         try
         {
-            var courseDaos = await (courseRepository.GetCoursesByTypeAsync(TrainingType.AllKeysTest));
-            var courseDao = courseDaos.FirstOrDefault(c => c.Id == id);
-            if (courseDao != null)
+            var courseDaos = await courseRepository.GetCoursesByTypeAsync(accountId, type);
+            var courses = mapper.Map<IEnumerable<CourseDto>>(courseDaos);
+            var coursesByTypes = courses.ToList();
+
+            // If no courses found for user, add a default course
+            if (!coursesByTypes.AsEnumerable().Any())
             {
-                var course = mapper.Map<CourseDto>(courseDao);
-                return course;
+                var setting = new CourseSetting
+                {
+                    Minutes = 120,
+                    NewKeysPerStep = 1,
+                    PracticeTextLength = 74,
+                    TargetStats = new StatsBase { Wpm = 50, Accuracy = 90 }
+                };
+                CourseDto? course = null;
+                switch (type)
+                {
+                    case TrainingType.AllKeysTest:
+                    case TrainingType.SpeedTest:
+                        course = await GeneratePracticeCourse(accountId, type, setting);
+                        break;
+                }
+
+                if (course != null)
+                {
+                    coursesByTypes.Add(course);
+                }
+            }
+            else
+            {
+                // If courses are found, we need add lesson for practice course
+                if (type is TrainingType.AllKeysTest or TrainingType.SpeedTest)
+                {
+                    foreach (var item in coursesByTypes)
+                    {
+                        var practiceCourse = _courseFactory.GetInitializedCourse(item);
+                        if (practiceCourse != null)
+                        {
+                            item.Lessons = practiceCourse.Lessons;
+                        }
+                    }
+                }
             }
 
-            ProcessResult.AddError($"Course with ID {id} not found");
-            return null;
+            return coursesByTypes;
         }
-        catch (Exception e)
+        catch (Exception ex)
         {
-            Console.WriteLine(e);
-            throw;
-        }
-    }
-    public async Task<CourseDto?> GetDiskTestCourse(Guid id)
-    {
-        try
-        {
-            var courseDaos = await (courseRepository.GetCoursesByTypeAsync(TrainingType.SpeedTest));
-            var courseDao = courseDaos.FirstOrDefault(c => c.Id == id);
-            if (courseDao != null)
-            {
-                var course = mapper.Map<CourseDto>(courseDao);
-                return course;
-            }
-
-            ProcessResult.AddError($"Course with ID {id} not found");
-            return null;
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine(e);
-            throw;
+            ProcessResult.AddException(ex);
+            return new List<CourseDto>();
         }
     }
 
@@ -220,22 +189,6 @@ public class CourseService(ICourseRepository courseRepository, IAccountRepositor
         }
     }
 
-    public Task<CourseDto> GenerateDiskTestCourse(CourseSetting settings)
-    {
-        var course = new CourseDto
-        {
-            Id = Guid.NewGuid(),
-            Name = "DiskTestCourse",
-            Settings = settings,
-            Lessons = new List<Lesson>(),
-            Type = TrainingType.Course,
-            LessonDataUrl = string.Empty,
-            Description = "This is a disk test (speed test) course for new typists"
-        };
-
-        return Task.FromResult(course);
-    }
-
     public Task<CourseDto> GenerateBeginnerCourse(CourseSetting settings)
     {
         var course = new CourseDto
@@ -248,6 +201,35 @@ public class CourseService(ICourseRepository courseRepository, IAccountRepositor
             LessonDataUrl = string.Empty,
             Description = "This is a beginner course for new typists"
         };
+
+        return Task.FromResult(course);
+    }
+
+    public Task<CourseDto> GeneratePracticeCourse(int accountId, TrainingType type, CourseSetting settings)
+    {
+        var courseFileName = type switch
+        {
+            TrainingType.AllKeysTest => TypingMasterConstants.AllKeysCourseName,
+            TrainingType.SpeedTest => TypingMasterConstants.SpeedTestCourseName,
+            _ => throw new ArgumentOutOfRangeException(nameof(type), type, null)
+        };
+        var course = new CourseDto
+        {
+            Id = Guid.NewGuid(),
+            AccountId = accountId,
+            Name = courseFileName,
+            Settings = settings,
+            Lessons = new List<Lesson>(),
+            Type = type,
+            LessonDataUrl = GetDefaultLessonDataUrl(type, courseFileName),
+            Description = "This is a disk test (speed test) course for new typists"
+        };
+
+        var courseTemplate = _courseFactory.GetInitializedCourse(course);
+        if (courseTemplate != null)
+        {
+            course.Lessons= courseTemplate.Lessons;
+        }
 
         return Task.FromResult(course);
     }
@@ -298,32 +280,32 @@ public class CourseService(ICourseRepository courseRepository, IAccountRepositor
             }
 
             // Process the course with the provided function
-            course.GetPracticeLesson(lessonId, stats);
+            return course.GetPracticeLesson(lessonId, stats);
         }
         catch (Exception ex)
         {
             ProcessResult.AddException(ex);
             return null;
         }
-        return await ProcessCourse(courseId, stats, (course, targetStats) => course.GetPracticeLesson(lessonId, targetStats));
     }
 
     // Helper method to get default lesson data URL based on course type and name
     private string GetDefaultLessonDataUrl(TrainingType type, string name)
     {
         var defaultUrlsSection = configuration.GetSection("CourseSettings:DefaultLessonUrls");
-        
+
         // Try to get the URL based on the course name first
         var url = defaultUrlsSection[name];
-        
+
         // If not found, use a fallback approach based on type
         if (string.IsNullOrWhiteSpace(url))
         {
             url = (type, name) switch
             {
-                (TrainingType.Course, "BeginnerCourse") => "Resources/LessonData/beginner-course-lessons.json",
-                (TrainingType.Course, "AdvancedLevelCourse") => "Resources/LessonData/advanced-level-course-lessons.json",
-                (TrainingType.AllKeysTest,"AllKeysTest" ) => "Resources/LessonData/all-keys-test-course-lessons.json",
+                (TrainingType.Course, TypingMasterConstants.BeginnerCourseName) => "Resources/LessonData/beginner-course-lessons.json",
+                (TrainingType.Course, TypingMasterConstants.AdvancedLevelCourseName) => "Resources/LessonData/advanced-level-course-lessons.json",
+                (TrainingType.AllKeysTest, TypingMasterConstants.AllKeysCourseName) => "Resources/LessonData/all-keys-test-course-lessons.json",
+                (TrainingType.SpeedTest, TypingMasterConstants.SpeedTestCourseName) => "Resources/LessonData/disk-test-course-lessons.json",
                 _ => string.Empty
             };
         }
